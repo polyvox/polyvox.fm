684bca0ac3d040e3b6447f03ebabe4a3ff4dfaf1
Curtis Schlak <realistschuckle@gmail.com>
2015-12-07 19:49:36 -0600

    Trying to get a release built.

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :polyvox, Polyvox.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "polyvox.fm", port: 4000],
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
