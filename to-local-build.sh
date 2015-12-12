d7c03486897a1fc7ce6c5f8342f130592606bd6b
Curtis Schlak <realistschuckle@gmail.com>
2015-12-12 12:18:12 -0600

    This, I think, solves the build problem thing.

--- to-local-build.sh
+++ to-local-build.sh
@@ -13,7 +13,9 @@ move-it-all () {
 		rm ./archive.zip
 		vagrant ssh -c "unzip /home/vagrant/archive.zip -d /home/vagrant/polyvox"
 		vagrant ssh -c "rm -f /home/vagrant/archive.zip /home/vagrant/polyvox/Vagrantfile /home/vagrant/polyvox/to-local-build.sh"
-		vagrant ssh -c "pushd ~/polyvox && npm install && mix deps.get && mix deps.compile && mix ecto.reset && brunch build --production && mix phoenix.digest && mix phoenix.server"
+		vagrant ssh -c "pushd ~/polyvox && npm install && mix deps.get && mix compile && mix ecto.reset && brunch build --production"
+		vagrant ssh -c "pushd ~/polyvox && mix phoenix.digest"
+		vagrant ssh -c "pushd ~/polyvox && mix phoenix.server"
 }
 
 move-it-all
