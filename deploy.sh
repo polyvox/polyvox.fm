be2d02f270e739c327ec06d7e3ab8184e28eaa07
Curtis Schlak <realistschuckle@gmail.com>
2015-12-13 10:27:44 -0600

    Add /latest and /latest-rel soft links to the website directory so that we can actually increment version numbers\!

--- deploy.sh
+++ deploy.sh
@@ -20,15 +20,22 @@ if [ ! -f $SRC_PATH ]; then
 fi
 
 DEST_DIR="/var/www/polyvox.fm/polyvox-$1"
+PRIV_DIR="${DEST_DIR}/lib/polvyox-$1/priv/static"
 DEST_PATH="${DEST_DIR}/polyvox-$1.tar.gz"
-CLEAN_COMMAND="rm -rf ${DEST_DIR}"
+
+CLEAN_COMMAND="rm -rf ${DEST_DIR} /var/www/polyvox.fm/latest"
 STAGE_COMMAND="mkdir -p ${DEST_DIR}"
 UNTAR_COMMAND="pushd ${DEST_DIR} && tar xvzf polyvox-$1.tar.gz"
+CLEANUP_COMMAND="rm -f ${DEST_PATH}"
+LINK_COMMAND="ln -s ${DEST_DIR} /var/www/polyvox.fm/latest-rel"
+LINKWWW_COMMAND="ln -s ${PRIV_DIR} /var/www/polyvox.fm/latest"
 
 ssh curweb1.curtissimo.com "sudo /var/www/polyvox.fm/stop-site.sh"
 ssh curweb1.curtissimo.com "${CLEAN_COMMAND}"
 ssh curweb1.curtissimo.com "${STAGE_COMMAND}"
 scp ${SRC_PATH} curweb1.curtissimo.com:${DEST_DIR}
 ssh curweb1.curtissimo.com "${UNTAR_COMMAND}"
-ssh curweb1.curtissimo.com "rm -f ${DEST_PATH}"
+ssh curweb1.curtissimo.com "${CLEANUP_COMMAND}"
+ssh curweb1.curtissimo.com "${LINK_COMMAND}"
+ssh curweb1.curtissimo.com "${LINKWWW_COMMAND}"
 ssh curweb1.curtissimo.com "sudo /var/www/polyvox.fm/start-site.sh"
