3f25b67e27b7d2e3c5e4eee3b6c41bc382006818
Curtis Schlak <realistschuckle@gmail.com>
2015-12-16 15:04:11 -0600

    Flex fix on insiders and a little CSS look and feel clean-up. Also, some hardening.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule PolyvoxMarketing.LayoutView do
   use PolyvoxMarketing.Web, :view
+
+  def version() do
+    {:ok, vsn} = :application.get_key(:polyvox_marketing, :vsn)
+    vsn |> List.to_string
+  end
 end
