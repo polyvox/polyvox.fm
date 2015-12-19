420e3b8814c5395c48ec574b8ec5ecc72ea4a026
Curtis Schlak <realistschuckle@gmail.com>
2015-12-19 08:48:32 -0600

    Adding stuff to assets.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,21 +4,32 @@ import { Ripple } from "./ripple";
 const FIXED_NAVBAR = 'navbar-fixed-top';
 
 function fix_navbar () {
-	if (window.innerWidth < 768) {
-		$('nav').removeClass(FIXED_NAVBAR);
-	} else {
-		$('nav').addClass(FIXED_NAVBAR);				
-	}
+    if (window.innerWidth < 768) {
+	$('nav').removeClass(FIXED_NAVBAR);
+    } else {
+	$('nav').addClass(FIXED_NAVBAR);				
+    }
 };
 
 $(window).resize(fix_navbar);
 fix_navbar();
 
 $('#application-form').click(function () {
-	var other = document.querySelector('#application_priority_other');
-	if (other && other.checked) {
-		document.querySelector('#application_priority').disabled = false;
-	} else {
-		document.querySelector('#application_priority').disabled = true;
-	}
+    var other = document.querySelector('#application_priority_other');
+    if (other && other.checked) {
+	document.querySelector('#application_priority').disabled = false;
+    } else {
+	document.querySelector('#application_priority').disabled = true;
+    }
 });
+
+var imp = document.createElement('link');
+imp.rel = 'import';
+imp.href = '/logo.svg';
+imp.addEventListener('load', function (e) {
+    alert('hello!');
+});
+imp.addEventListener('error', function (e) {
+    alert('good-bye!');
+});
+document.head.appendChild(imp);
