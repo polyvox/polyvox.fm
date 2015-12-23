627fb01671ae5793d7ce62f398131611b37db31b
Curtis Schlak <realistschuckle@gmail.com>
2015-12-23 14:50:27 -0600

    Some changes for that new version of shit.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,1 +1,11 @@
 import "deps/phoenix_html/web/static/js/phoenix_html";
+
+function showRatio() {
+    var footer = document.getElementsByTagName('footer')[0];
+    footer.innerHTML = window.innerWidth + '/' + window.innerHeight + '=' + (window.innerWidth / window.innerHeight);
+}
+
+showRatio();
+
+window.addEventListener('resize', showRatio);
+
