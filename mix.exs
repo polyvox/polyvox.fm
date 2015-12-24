fb5a6fb5d114aee7b58d029d3dcf07c6dbb1ac45
Curtis Schlak <realistschuckle@gmail.com>
2015-12-23 18:38:20 -0600

    Version 0.1.2 that has lots of pretty new stuff rather than yellow ripple.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PolyvoxMarketing.Mixfile do
 
   def project do
     [app: :polyvox_marketing,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,11 +1,22 @@
 import "deps/phoenix_html/web/static/js/phoenix_html";
 
-function showRatio() {
-    var footer = document.getElementsByTagName('footer')[0];
-    footer.innerHTML = window.innerWidth + '/' + window.innerHeight + '=' + (window.innerWidth / window.innerHeight);
-}
+function firstPage() {
+    var signUp = document.getElementById('sign-up'),
+        story = document.getElementById('story'),
+        logoHolder = document.getElementById('logo-holder'),
+        combinedHeight = signUp.clientHeight + story.clientHeight,
+        windowHeight = window.innerHeight,
+        windowWidth = window.innerWidth;
 
-showRatio();
+    if (windowWidth < 768 && windowHeight - combinedHeight < 220) {
+        logoHolder.style.height = windowHeight + 'px';
+    } else if (windowHeight - combinedHeight < 300) {
+        logoHolder.style.height = windowHeight + 'px';
+    } else {
+        logoHolder.style.height = (windowHeight - combinedHeight) + 'px';
+    }
+};
 
-window.addEventListener('resize', showRatio);
+window.addEventListener('resize', firstPage);
 
+setTimeout(firstPage, 100);
