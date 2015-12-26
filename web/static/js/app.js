1abccc3b1e7f87550b656aa3170bbd437340e23b
Curtis Schlak <realistschuckle@gmail.com>
2015-12-26 10:15:49 -0600

    Start ES for listening.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,1 +1,10 @@
 import "deps/phoenix_html/web/static/js/phoenix_html";
+
+var listen = document.getElementById('listen');
+listen.addEventListener('click', e => {
+    textButtonToPlayButton();
+});
+
+function textButtonToPlayButton() {
+    console.log('hi');
+}
